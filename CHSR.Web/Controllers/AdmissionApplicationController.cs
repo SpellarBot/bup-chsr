a3a39477f0a00b34d9db1a9a1d8dc56a4e502eff
Khandaker Arif Ahmed <khandaker.ahmed@nascenia.com>
2019-04-09 18:37:15 +0600

    AdmissionApplicationForm Setup

---
+++ CHSR.Web/Controllers/AdmissionApplicationController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace CHSR.Web.Controllers
+{
+    public class AdmissionApplicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("BasicInfo");
+        }
+
+        [HttpGet]
+        public IActionResult BasicInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BasicInfo(string abc)
+        {
+            return RedirectToAction("AttachDocuments");
+        }
+
+
+        [HttpGet]
+        public IActionResult AttachDocuments(string abc)
+        {
+            return View();
+        }
+    }
+}
