fcd832922b53ed9ec1b86508e5238444f965b03b
sayedurrahman <sayedur.rahmans@gmail.com>
2019-05-03 18:04:29 +0600

    Repo added

---
+++ CHSR.Repository/CHSRRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CHSR.Repository
+{
+    public class CHSRRepository<T> : IGenericRepository<T> where T : class
+    {
+        //private readonly CHSRContext _context;
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            //_context.Institutes.ToListAsync()
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
